dac9f77a84dfd5a44c97787b4b8f81d7abf2849f
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-09-07 22:21:33 +0200

    Add `general.py` to `Developer`cog

---
+++ cogs/developer/general.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from discord.ext import commands
+from jishaku.codeblocks import codeblock_converter as CodeblockConverter
+
+from utils import BaseCog
+
+if TYPE_CHECKING:
+    from bot import Harmony
+    from utils import Context
+
+
+class General(BaseCog):
+    def __init__(self, bot: Harmony) -> None:
+        super().__init__(bot)
+        self.bot = bot
+
+    @commands.command()
+    async def eval(self, ctx: Context, *, code: CodeblockConverter):  # type: ignore
+        await ctx.invoke(self.bot.get_command("jishaku python"), argument=code)  # type: ignore
