49f35fc3b51bfa42f74a1af4c47a5951bbd45b5a
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-11-25 19:14:34 +0100

    Changes to paginator

--- utils/paginator.py
+++ utils/paginator.py
@@ -8,29 +8,29 @@ class PaginatorView(discord.ui.View):
         super().__init__(timeout=300)
         self.paginator = paginator
 
-    @discord.ui.button(label="<-", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="<<", style=discord.ButtonStyle.blurple)
     async def prev(self, interaction: discord.Interaction, _):
-        p = self.paginator
+        page = self.paginator
 
-        if p.index == 0:
+        if page.index == 0:
             pass
 
         else:
-            p.previous_page()
+            page.previous_page()
 
-        await interaction.response.edit_message(embed=p.current_page)
+        await interaction.response.edit_message(embed=page.current_page)
 
-    @discord.ui.button(label="->", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label=">>", style=discord.ButtonStyle.blurple)
     async def next(self, interaction: discord.Interaction, _):
-        p = self.paginator
+        page = self.paginator
 
-        if p.index + 1 == p.length:
+        if page.index + 1 == page.length:
             pass
 
         else:
-            p.next_page()
+            page.next_page()
 
-        await interaction.response.edit_message(embed=p.current_page)
+        await interaction.response.edit_message(embed=page.current_page)
 
 
 class Paginator:
@@ -43,7 +43,10 @@ class Paginator:
 
     @property
     def current_page(self) -> discord.Embed:
-        return self._current_page.set_footer(text=f"Page: {self.user_page}/{self.length}")
+        embed = self._current_page
+        if len(self.embeds) < 1:
+            return embed
+        return embed.set_footer(text=f"Page: {self.user_page}/{self.length}")
 
     @current_page.setter
     def current_page(self, embed: discord.Embed) -> None:
@@ -72,4 +75,5 @@ class Paginator:
         self.current_page = self.embeds[index - 1]
 
     async def start(self, channel: discord.abc.MessageableChannel) -> None:
-        await channel.send(embed=self.current_page, view=self.view)
+        view = self.view if len(self.embeds) > 1 else discord.utils.MISSING
+        await channel.send(embed=self.current_page, view=view)
