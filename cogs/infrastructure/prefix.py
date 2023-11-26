76e16687488e00224750846e0d0da43db1d6c163
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2023-11-26 13:20:29 +0100

    Fix imports

--- cogs/infrastructure/prefix.py
+++ cogs/infrastructure/prefix.py
@@ -6,12 +6,11 @@ import discord
 from discord.ext import commands
 
 from config import DEFAULT_PREFIX
-from utils import ErrorEmbed, PrimaryEmbed, SuccessEmbed
-from utils.cog import BaseCog
+from utils import ErrorEmbed, PrimaryEmbed, SuccessEmbed, BaseCog
 
 if TYPE_CHECKING:
     from bot import Harmony
-    from utils.context import Context
+    from utils import Context
 
 
 class Prefix(BaseCog):
