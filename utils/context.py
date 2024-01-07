29312b8dc9aaba26299da66a7e91baff3c04ff41
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-01-07 19:08:46 +0100

    Fix custom Context

--- utils/context.py
+++ utils/context.py
@@ -1,36 +1,19 @@
-from typing import Self, overload
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, overload
 
 import discord
 from discord.ext import commands
 from discord.ext.commands import Context as DiscordContext
 from discord.ext.commands.core import Command
 
-from bot import Harmony
+if TYPE_CHECKING:
+    from bot import Harmony  # noqa: F401
 
 
-class Context(DiscordContext[Harmony]):
+class Context(DiscordContext["Harmony"]):
     guild: discord.Guild
     command: commands.Command
-
-    @classmethod
-    def from_context(cls, context: DiscordContext) -> Self:
-        return cls(
-            message=context.message,
-            bot=context.bot,
-            view=context.view,
-            args=context.args,
-            kwargs=context.kwargs,
-            prefix=context.prefix,
-            command=context.command,
-            invoked_with=context.invoked_with,
-            invoked_parents=context.invoked_parents,
-            invoked_subcommand=context.invoked_subcommand,
-            subcommand_passed=context.subcommand_passed,
-            command_failed=context.command_failed,
-            current_parameter=context.current_parameter,
-            current_argument=context.current_argument,
-            interaction=context.interaction,
-        )
 
     @property
     def clean_prefix(self) -> str:
