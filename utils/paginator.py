34f149f3095ba2be5a9730effc638cfaaaa8b9fd
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-01-07 19:09:10 +0100

    Add interaction check, as well as revamp a little bit of the paginator

--- utils/paginator.py
+++ utils/paginator.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 import discord
+from discord.interactions import Interaction
 
 
 class PaginatorView(discord.ui.View):
     def __init__(self, paginator: Paginator) -> None:
         super().__init__(timeout=300)
         self.paginator = paginator
+        self.page.label = f"1/{paginator.length}"
 
     def update(self):
         self.prev.disabled = False
@@ -17,6 +19,16 @@ class PaginatorView(discord.ui.View):
 
         elif self.paginator.user_page == self.paginator.length:
             self.next.disabled = True
+
+        self.page.label = f"{self.paginator.user_page}/{self.paginator.length}"
+
+    async def interaction_check(self, interaction: Interaction) -> bool:
+        if author := self.paginator.author:
+            if author != interaction.user:
+                await interaction.response.send_message("This is not for you.", ephemeral=True)
+                return False
+
+        return True
 
     @discord.ui.button(label="<<", style=discord.ButtonStyle.blurple, disabled=True)
     async def prev(self, interaction: discord.Interaction, _):
@@ -31,6 +43,10 @@ class PaginatorView(discord.ui.View):
         self.update()
         await interaction.response.edit_message(embed=page.current_page, view=self)
 
+    @discord.ui.button(label="", style=discord.ButtonStyle.gray, disabled=True)
+    async def page(self, *_):
+        pass
+
     @discord.ui.button(label=">>", style=discord.ButtonStyle.blurple)
     async def next(self, interaction: discord.Interaction, _):
         page = self.paginator
@@ -44,10 +60,16 @@ class PaginatorView(discord.ui.View):
         self.update()
         await interaction.response.edit_message(embed=page.current_page, view=self)
 
+    @discord.ui.button(label="\N{WASTEBASKET}\N{VARIATION SELECTOR-16}", style=discord.ButtonStyle.red)
+    async def delete(self, interaction: discord.Interaction, _):
+        assert interaction.message
+        await interaction.message.delete()
+
 
 class Paginator:
-    def __init__(self, embeds: list[discord.Embed]) -> None:
+    def __init__(self, embeds: list[discord.Embed], author: discord.User | discord.Member | None = None) -> None:
         self.embeds = embeds
+        self.author = author
         self._current_page = embeds[0]
         self.page = 0
 
@@ -55,10 +77,7 @@ class Paginator:
 
     @property
     def current_page(self) -> discord.Embed:
-        embed = self._current_page
-        if len(self.embeds) == 1:
-            return embed
-        return embed.set_footer(text=f"Page: {self.user_page}/{self.length}")
+        return self._current_page
 
     @current_page.setter
     def current_page(self, embed: discord.Embed) -> None:
