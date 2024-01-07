40b0d3f4c5f2ed236e586cd90b9fc1a053b476ec
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-01-07 19:02:22 +0100

    Add IPC

--- cogs/infrastructure/__init__.py
+++ cogs/infrastructure/__init__.py
@@ -6,12 +6,13 @@ from .error_handler import ErrorHandler
 from .help import Help
 from .prefix import Prefix
 from .statistics import Statistics
+from .ipc import IPC
 
 if TYPE_CHECKING:
     from bot import Harmony
 
 
-class Infrastructure(Prefix, ErrorHandler, Help, Statistics):
+class Infrastructure(Prefix, ErrorHandler, Help, Statistics, IPC):
     def __init__(self, bot: Harmony) -> None:
         super().__init__(bot)
 
