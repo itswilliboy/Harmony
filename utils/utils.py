21e072372d05c80d7849a637923213e1d738b1ff
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-01-09 01:20:24 +0100

    Add util function

--- utils/utils.py
+++ utils/utils.py
@@ -1,1 +1,19 @@
+from types import NoneType
+
+import discord
+from discord.ext import commands
 from discord.utils import as_chunks as chunk  # noqa: F401
+
+from . import Context
+
+
+def _check(ctx: Context) -> str:
+    if ref := ctx.message.reference:
+        if not isinstance(ref.resolved, (discord.DeletedReferencedMessage, NoneType)):
+            return ref.resolved.content
+
+    return ""
+
+argument_or_reference = commands.parameter(default=lambda ctx: _check(ctx))
+
+
