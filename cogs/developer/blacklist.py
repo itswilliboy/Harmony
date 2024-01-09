590fc4db7be5f96f01e8cd7dc8333c1eaf3a5fed
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-01-09 01:18:10 +0100

    Add guild blacklist

--- cogs/developer/blacklist.py
+++ cogs/developer/blacklist.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import datetime
 
 from typing import TYPE_CHECKING, Self
 
@@ -14,7 +15,7 @@ if TYPE_CHECKING:
 
 
 class BlacklistItem:
-    """Represents an item on the blacklist"""
+    """Represents an item on the blacklist."""
 
     def __init__(self, cog: Blacklist, record: Record) -> None:
         self.cog = cog
@@ -61,7 +62,8 @@ class BlacklistItem:
             guild.id,
             self.user_id,
         )
-        self.__init__(self.cog, updated)  # type: ignore
+        assert updated
+        self.__init__(self.cog, updated)
         return self
 
     async def remove_guild(self, guild: discord.abc.Snowflake) -> Self:
@@ -87,6 +89,28 @@ class BlacklistItem:
         return self
 
 
+class GuildBlacklistItem:
+    def __init__(self, record: Record) -> None:
+        self._guild_id: int = record["guild_id"]
+        self._reason: str = record["reason"]
+        self._timestamp: datetime.datetime = record["timestamp"]
+
+    @property
+    def guild_id(self) -> int:
+        """Returns the ID of the guild that is blacklisted."""
+        return self._guild_id
+
+    @property
+    def reason(self) -> str:
+        """Returns the reason for the blacklist."""
+        return self._reason
+
+    @property
+    def timestamp(self) -> datetime.datetime:
+        """Returns the date and time of blacklisting."""
+        return self._timestamp
+
+
 class Flags(commands.FlagConverter, prefix="--", delimiter=" "):
     guild: discord.Guild | None = None
     reason: str | None = None
@@ -96,23 +120,22 @@ class Blacklist(BaseCog):
     def __init__(self, bot: Harmony) -> None:
         super().__init__(bot)
         self.blacklist: dict[int, BlacklistItem] = {}
+        self.guild_blacklist: dict[int, GuildBlacklistItem] = {}
         bot.loop.create_task(self.fill_cache())
         bot.add_check(self.blacklist_check, call_once=True)
+
+    @staticmethod
+    def blacklist_check(ctx: Context) -> bool:
+        return not ctx.is_blacklisted()
 
     async def fill_cache(self) -> None:
         records = await self.bot.pool.fetch("SELECT * FROM blacklist")
         for record in records:
             self.blacklist[record["user_id"]] = BlacklistItem(self, record)
 
-    def blacklist_check(self, ctx: Context) -> bool:
-        if ctx.author.id in self.blacklist.keys():
-            item = self.blacklist[ctx.author.id]
-            if item.is_global:
-                return False
-            else:
-                if ctx.guild.id in item.guild_ids:
-                    return False
-        return True
+        records = await self.bot.pool.fetch("SELECT * FROM guild_blacklist")
+        for record in records:
+            self.guild_blacklist[record["guild_id"]] = GuildBlacklistItem(record)
 
     async def add_blacklist(
         self, user: discord.User, *, guild: discord.Guild | None = None, reason: str | None = None
@@ -120,9 +143,9 @@ class Blacklist(BaseCog):
         """Create a blacklist for a user, optionally in a specific guild."""
         if guild:
             query = """
-            INSERT INTO blacklist (user_id, guild_ids, reason, global) 
-            VALUES ($1, $2, $3, $4) 
-            RETURNING *
+                INSERT INTO blacklist (user_id, guild_ids, reason, global) 
+                VALUES ($1, $2, $3, $4) 
+                RETURNING *
             """
 
             record = await self.bot.pool.fetchrow(query, user.id, [guild.id], reason, False)
@@ -132,7 +155,7 @@ class Blacklist(BaseCog):
                 INSERT INTO blacklist (user_id, reason, global) 
                 VALUES ($1, $2, $3) 
                 RETURNING *
-                """
+            """
 
             record = await self.bot.pool.fetchrow(query, user.id, reason, True)
 
@@ -147,6 +170,28 @@ class Blacklist(BaseCog):
         query = "DELETE FROM blacklist WHERE user_id = $1"
         await self.bot.pool.execute(query, user.id)
         del self.blacklist[user.id]
+
+    async def add_guild_blacklist(self, guild: discord.Guild, reason: str) -> GuildBlacklistItem:
+        """Add a guild to the blacklist."""
+        query = """
+            INSERT INTO guild_blacklist VALUES ($1, $2, $3)
+            RETURNING *
+        """
+        record = await self.bot.pool.fetchrow(query, guild.id, reason, datetime.datetime.now())
+
+        assert record
+        item = GuildBlacklistItem(record)
+        self.guild_blacklist[guild.id] = item
+
+        return item
+
+    async def remove_guild_blacklist(self, guild: discord.Guild) -> None:
+        """Unblacklist a guild."""
+        query = """
+            DELETE FROM guild_blacklist WHERE guild_id = $1
+        """
+        await self.bot.pool.execute(query, guild.id)
+        self.guild_blacklist.pop(guild.id, None)
 
     @commands.group(name="blacklist", hidden=True)
     async def blacklist_(self, ctx: Context) -> None:
@@ -215,4 +260,61 @@ class Blacklist(BaseCog):
             .set_footer(text=f"{str(user)} | {user.id}")
         )
 
+        await ctx.send(embed=embed)
+
+    @commands.group(name="guild_blacklist", aliases=["gblacklist"])
+    async def guild_blacklist_(self, ctx: Context):
+        if not ctx.invoked_subcommand:
+            raise GenericError("Invoke a valid subcommand.")
+
+    @guild_blacklist_.command(name="add")
+    async def g_add(
+        self,
+        ctx: Context,
+        guild_id: int = commands.param(default=lambda ctx: ctx.guild.id),
+        *,
+        reason: str = "No reason given."
+    ):
+        guild = self.bot.get_guild(guild_id)
+
+        if guild is None:
+            raise GenericError("Couldn't find that guild.")
+
+        await self.add_guild_blacklist(guild, reason)
+        embed = SuccessEmbed(description=f"Successfully blacklisted `{guild.name}`.")
+        await ctx.send(embed=embed)
+
+    @guild_blacklist_.command(name="status")
+    async def g_status(self, ctx: Context, guild_id: int):
+        item = self.guild_blacklist.get(guild_id)
+
+        if item is None:
+            raise GenericError("That guild isn't blacklisted.")
+
+        guild = self.bot.get_guild(guild_id)
+        assert guild
+
+        embed = PrimaryEmbed(
+            title="Guild Blacklisted",
+            description=f"Reason: {item.reason}\nSince: {discord.utils.format_dt(item.timestamp, 'D')}"
+        )
+
+        if guild.icon:
+            embed.set_thumbnail(url=guild.icon)
+
+        await ctx.send(embed=embed)
+
+    @guild_blacklist_.command(name="remove")
+    async def g_remove(self, ctx: Context, guild_id: int):
+        item = self.guild_blacklist.get(guild_id)
+
+        if item is None:
+            raise GenericError("That guild isn't blacklisted.")
+
+        guild = self.bot.get_guild(guild_id)
+        assert guild
+
+        await self.remove_guild_blacklist(guild)
+
+        embed = SuccessEmbed(description=f"Successfully unblacklisted `{guild.name}`.")
         await ctx.send(embed=embed)
