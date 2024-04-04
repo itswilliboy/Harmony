ebf7759813391058e918402b42eb27cd700e35f5
ItsWilliboy <83978878+itswilliboy@users.noreply.github.com>
2024-04-05 01:01:36 +0200

    Add reload command for developers

--- cogs/developer/general.py
+++ cogs/developer/general.py
@@ -19,3 +19,22 @@ class General(BaseCog):
     @commands.command(aliases=["e"])
     async def eval(self, ctx: Context, *, code: CodeblockConverter):  # type: ignore
         await ctx.invoke(self.bot.get_command("jishaku python"), argument=code)  # type: ignore
+
+    @commands.command(aliases=["r"])
+    async def reload(self, ctx: Context, extension: str | None = None):
+        """Reloads one or more extensions."""
+        try:
+            if extension is None:
+                for ext in list(self.bot.extensions).copy():
+                    if ext in ("cogs.infrastructure", "jishaku"):
+                        continue
+                    await self.bot.reload_extension(ext)
+
+            else:
+                await self.bot.reload_extension(extension)
+
+        except Exception as exc:
+            await ctx.message.add_reaction("\N{CROSS MARK}")
+            self.bot.log.error(f"Something went wrong when trying to reload {ext or extension}: ", exc_info=exc)
+
+        await ctx.message.add_reaction("\N{OK HAND SIGN}")
